bd1542680db46cef3670be6e9d0285addb1ecee4
Ben <barmiste@gmail.com>
2019-08-17 17:13:17 -0700

    Fixup missing user data command.

--- ec2-user-data/user-data.sh
+++ ec2-user-data/user-data.sh
@@ -4,4 +4,5 @@ yum install -y ruby
 cd /home/ec2-user
 curl -O https://aws-codedeploy-us-west-1.s3.amazonaws.com/latest/install
 chmod +x ./install
+./install auto
 yum install -y java-1.8.0-openjdk
