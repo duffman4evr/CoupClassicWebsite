46d0013b4b7268efddc338c5618da0d1b55175ef
Jdale28 <jordan.dale2@gmail.com>
2019-09-01 16:12:15 -0400

    Initial merge request; Logo changed

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://media.giphy.com/media/3oAt21Fnr4i54uK8vK/giphy.gif"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Coup
         </a>
       </header>
     </div>
