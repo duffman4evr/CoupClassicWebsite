e215b56c6281645a9dbb7e550669f9d64ed23de3
Jdale28 <jordan.dale2@gmail.com>
2019-09-03 23:16:37 -0400

    Added initial Navbar and styling

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,23 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+const Global = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=PT+Sans|Raleway');
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'PT Sans';
+  }
+`
+
+const NavBarStyles = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 6vh;
+  font-size: 2rem;
+  background-color: #1A090D;
+  font-family: "Raleway";
+  text-transform: uppercase;
+  z-index: 2;
+  a {
+    text-decoration: none;
+    padding-left: 2vw;
+    color: white;
+    &:hover {
+      color: #6B6570;
+    }
+  }
+  .left {
+    display: flex;
+    flex-direction: row;
+    width: 35vw;
+  }
+  .right {
+    margin-right: 2vw;
+    width: 30vw;
+    display: flex;
+    justify-content: space-around;
+  }
+`
 
 function App() {
   return (
     <div className="App">
+      <Global />
+      <NavBarStyles>
+        <div className="left">
+          <a href="#about">About Us</a>
+          <a href="#history">Our History</a>
+          <a href="#loot">Loot Rules</a>
+        </div>
+        <div className="right">
+          <a href="#dkp">Current DKP</a>
+          <a href="#contact">Contact Us</a>
+        </div>
+      </NavBarStyles>
       <header className="App-header">
-        <img src={"https://media.giphy.com/media/3oAt21Fnr4i54uK8vK/giphy.gif"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Coup
-        </a>
+        <img src={"https://media.giphy.com/media/3oAt21Fnr4i54uK8vK/giphy.gif"} />
+        <h1>Welcome to Coup</h1>
+        <p>An alliance guild on Atiesh</p>
       </header>
     </div>
   );
