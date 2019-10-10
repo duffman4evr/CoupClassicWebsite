8b1642c36253c223a1725c45717760ecaa4daeec
Tony Allain <tallain@blizzard.com>
2019-10-10 14:37:12 -0700

    old image
    * to keep font configurable, removing any images with baked text

--- src/web/src/components/HomePage.js
+++ src/web/src/components/HomePage.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
 import { Image } from 'semantic-ui-react';
-import AboutUs from './AboutUs';
-import ContactUs from './ContactUs';
 import ragnaros from '../media/ragnaros.jpg';
-import Recruitment from './Recruitment';
-import ScrollableAnchor from 'react-scrollable-anchor';
 
 class HomePage extends Component {
   render() {
     return (
       <div>
         <Image src={ragnaros} centered fluid />
-
-        <ScrollableAnchor id={'about'}>
-          <AboutUs />
-        </ScrollableAnchor>
-
-        <ScrollableAnchor id={'recruitment'}>
-          <Recruitment />
-        </ScrollableAnchor>
-
-        <ScrollableAnchor id={'contact'}>
-          <ContactUs />
-        </ScrollableAnchor>
-
-        {/* This page will integrate with the app (hopefully) and be locked behind OAuth
-                <ScrollableAnchor id={"dkp"}>
-                    <CurrentDkp />
-                </ScrollableAnchor> */}
       </div>
     );
   }
