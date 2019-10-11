e80cc6910dee38d4ebc5dd5f89eddc4bb610e31f
Tony Allain <tallain@blizzard.com>
2019-10-11 15:02:02 -0700

    support for wowhead tooltips
    * pushing up tooltip support and 1 sample tooltip

--- src/web/src/components/ItemValues.js
+++ src/web/src/components/ItemValues.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import { Image } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Image, Container } from 'semantic-ui-react';
 import values from '../media/item_values.jpg';
 
-const ItemValues = () => <Image src={values} centered />;
+const wowhead = '$WowheadPower';
 
-export default ItemValues;
+export default class ItemValues extends Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+
+    script.id = 'tooltip-api';
+    script.src = 'https://wow.zamimg.com/widgets/power.js';
+
+    document.body.appendChild(script);
+
+    if (window[wowhead]) {
+      window[wowhead].init();
+    }
+  }
+
+  componentWillUnmount() {
+    const tooltipApi = document.getElementById('tooltip-api');
+
+    if (tooltipApi) {
+      tooltipApi.remove();
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <Image src={values} centered />
+      </Container>
+    );
+  }
+}
---
+++ src/web/src/components/Tooltip.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+const Tooltip = ({ itemId, dkp }) => (
+  <Container>
+    <a
+      href={`https://classic.wowhead.com/item=${itemId}`}
+      data-wowhead={`item=${itemId}&domain=classic`}
+      target='_blank'
+    />
+
+    <span style={{ color: '#FFFFFF' }}>
+      {' : '}
+      {dkp}
+      {' DKP'}
+    </span>
+  </Container>
+);
+
+export default Tooltip;
